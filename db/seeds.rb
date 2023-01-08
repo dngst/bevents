5aebfbd9a3b8609c487d60d3b26a000be9214ffa
dngst <dngst@protonmail.com>
2023-01-08 17:55:50 +0300

    Add event seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+50.times do
+	Event.create!(
+		title: Faker::Name.name_with_middle,
+		description: Faker::Quote.matz,
+		location: Faker::Mountain.name,
+		category: Faker::Book.genre,
+		date: Date.today,
+		start: Time.now,
+		end: Time.now + (60 * 60)
+	)
+end
+
+p "created #{Event.all.size} events"
