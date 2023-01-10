d4fddcb8c5487f177390f91987ffed0d78a6068b
dngst <dngst@protonmail.com>
2023-01-10 21:12:25 +0300

    Add annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :bigint           not null, primary key
+#  category    :string
+#  date        :date
+#  description :text
+#  end_time    :datetime
+#  location    :string
+#  start_time  :datetime
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Event < ApplicationRecord
 end
