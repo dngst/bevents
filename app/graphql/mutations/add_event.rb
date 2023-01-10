2f89a122dd2ef755600f029931236174a1ff89fb
dngst <dngst@protonmail.com>
2023-01-10 18:58:48 +0300

    Fix typo

--- app/graphql/mutations/add_event.rb
+++ app/graphql/mutations/add_event.rb
@@ -12,7 +12,7 @@ module Mutations
 
         { event: event }
       rescue ActiveRecord::RecordInvalid => e
-        GraphQL::ExecutionError.new("INvalid attributes for #{e.record.class}:"\
+        GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
           " #{e.record.errors.full_messages.join(', ')}")
       end
     end
